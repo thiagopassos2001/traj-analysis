4427bef6a494b6dc09dca6f8ecddcb9bf756234b
thiago <passos.thiago2001@gmail.com>
2026-04-13 20:51:20 -0300

    all lanes

--- run/FASP/get_conflicts.py
+++ run/FASP/get_conflicts.py
@@ -18,16 +18,16 @@ if __name__=="__main__":
         # File motorcycle space speed
         df_mss = pd.read_csv(os.path.join("data/collected/Dissertação/MotorcycleSpaceSpeed",f))
         # File motorcycle road section
-        df_mrs = pd.read_csv(os.path.join("data/collected/Dissertação/MotorcycleRoadSection",f))
+        df_mrs_OG = pd.read_csv(os.path.join("data/collected/Dissertação/MotorcycleRoadSection",f))
 
-        df_mrs = df_mrs[df_mrs["virtual_lane_type"]=="Corredor Principal"].copy()
+        df_mrs = df_mrs_OG[df_mrs_OG["virtual_lane_type"]=="Corredor Principal"].copy()
 
         # Filter 1
-        df_mrs = df_mrs[df_mrs["virtual_lane_type"]=="Corredor Principal"]
+        # df_mrs = df_mrs[df_mrs["virtual_lane_type"]=="Corredor Principal"] # commented by run all
         # Merge
         df_mrs = df_mrs.merge(df_mss,on=["id","frame"],how="left")
         # Calc prioritize (Y)
-        df_mrs = df_mrs.groupby(["id","virtual_lane_type"]).agg({
+        df_mrs = df_mrs.groupby(["id"]).agg({ # ,"virtual_lane_type" # commented by run all
             "frame":"count",
             "x_instant_speed_reference":"max",
             "space_headway":"min",
@@ -40,7 +40,7 @@ if __name__=="__main__":
             # "space_headway":"m2_headway",
             # "TTC":"m3_TTC"
             })
-        df_mrs = df_mrs[df_mrs["frame_count"]>=45]
+        df_mrs = df_mrs[df_mrs["frame_count"]>=20] # commented by run all default -> 45
         
         # Get Xs (remerge)
         # m1
@@ -56,6 +56,8 @@ if __name__=="__main__":
             "space_headway"
         ]],on=["id","x_instant_speed_reference"],how="left")
         df_m1 = df_m1.drop_duplicates(subset=["id","x_instant_speed_reference"])
+        # Get virtual_lane_type
+        df_m1 = df_m1.merge(df_mrs_OG,on=["id","frame"],how="left")
         # Mean parameters
         for p in ["x_instant_speed_reference","front_gap","space_headway"]:
             df_m1[p+"_mean"] = df_m1.apply(lambda row:df_mss[(df_mss["id"]==row["id"]) & (df_mss["frame"].between(row["frame"]-buffer_frame_mean,row["frame"]+buffer_frame_mean))][p].mean(),axis=1)
@@ -72,6 +74,8 @@ if __name__=="__main__":
             "x_instant_speed_reference"
         ]],on=["id","space_headway"],how="left")
         df_m2 = df_m2.drop_duplicates(subset=["id","space_headway"])
+        # Get virtual_lane_type
+        df_m2 = df_m2.merge(df_mrs_OG,on=["id","frame"],how="left")
         # Mean parameters
         for p in ["space_headway","front_gap","x_instant_speed_reference"]:
             df_m2[p+"_mean"] = df_m2.apply(lambda row:df_mss[(df_mss["id"]==row["id"]) & (df_mss["frame"].between(row["frame"]-buffer_frame_mean,row["frame"]+buffer_frame_mean))][p].mean(),axis=1)
@@ -90,21 +94,23 @@ if __name__=="__main__":
             "delta_instant_speed_x",
         ]],on=["id","TTC"],how="left")
         df_m3 = df_m3.drop_duplicates(subset=["id","TTC"])
+        # Get virtual_lane_type
+        df_m3 = df_m3.merge(df_mrs_OG,on=["id","frame"],how="left")
         # Mean parameters
         for p in ["TTC","front_gap","x_instant_speed_reference","x_instant_speed_front","delta_instant_speed_x"]:
             df_m3[p+"_mean"] = df_m3.apply(lambda row:df_mss[(df_mss["id"]==row["id"]) & (df_mss["frame"].between(row["frame"]-buffer_frame_mean,row["frame"]+buffer_frame_mean))][p].mean(),axis=1)
 
         # renames
-        df_m1.columns = [col+"_m1" if not col in ["id","frame_count","vehicle_type_reference"] else col for col in df_m1.columns]
-        df_m2.columns = [col+"_m2" if not col in ["id","frame_count","vehicle_type_reference"] else col for col in df_m2.columns]
-        df_m3.columns = [col+"_m3" if not col in ["id","frame_count","vehicle_type_reference"] else col for col in df_m3.columns]
+        df_m1.columns = [col+"_m1" if not col in ["id","frame_count","vehicle_type_reference","virutal_lane","virtual_lane_type"] else col for col in df_m1.columns]
+        df_m2.columns = [col+"_m2" if not col in ["id","frame_count","vehicle_type_reference","virutal_lane","virtual_lane_type"] else col for col in df_m2.columns]
+        df_m3.columns = [col+"_m3" if not col in ["id","frame_count","vehicle_type_reference","virutal_lane","virtual_lane_type"] else col for col in df_m3.columns]
         
         # df_concat = df_m1.merge(df_m2,on=["id","frame_count","vehicle_type_reference"],how="left").merge(df_m3,on=["id"],how="left")
         # df_concat.to_excel("Teste.xlsx",index=False)
         
-        df_m1.to_csv(os.path.join("data/collected/Dissertação/m1",f.replace(".json",".csv")),index=False)
-        df_m2.to_csv(os.path.join("data/collected/Dissertação/m2",f.replace(".json",".csv")),index=False)
-        df_m3.to_csv(os.path.join("data/collected/Dissertação/m3",f.replace(".json",".csv")),index=False)
+        df_m1.to_csv(os.path.join("data/collected/Dissertação/Interaction/All Lanes/m1",f.replace(".json",".csv")),index=False)
+        df_m2.to_csv(os.path.join("data/collected/Dissertação/Interaction/All Lanes/m2",f.replace(".json",".csv")),index=False)
+        df_m3.to_csv(os.path.join("data/collected/Dissertação/Interaction/All Lanes/m3",f.replace(".json",".csv")),index=False)
 
         print("OK",f)
 
